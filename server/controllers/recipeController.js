66b65fd5a8093318bb7d9eb1ecb703a5e7a9338e
shivarajkumar <shivaraj1654@gmail.com>
2022-06-24 18:37:02 +0530

    initial commit

---
+++ server/controllers/recipeController.js
@@ -0,0 +1,360 @@
+require('../models/database');
+const Category = require('../models/Category');
+const Recipe = require('../models/Recipe');
+
+/**
+ * GET /
+ * Homepage 
+*/
+exports.homepage = async(req, res) => {
+  try {
+    const limitNumber = 5;
+    const categories = await Category.find({}).limit(limitNumber);
+    const latest = await Recipe.find({}).sort({_id: -1}).limit(limitNumber);
+    const indian = await Recipe.find({ 'category': 'Indian' }).limit(limitNumber);
+    const american = await Recipe.find({ 'category': 'American' }).limit(limitNumber);
+    const chinese = await Recipe.find({ 'category': 'Chinese' }).limit(limitNumber);
+
+    const food = { latest, indian, american, chinese };
+
+    res.render('index', { title: 'Cooking Blog - Home', categories, food } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
+  }
+}
+
+/**
+ * GET /categories
+ * Categories 
+*/
+exports.exploreCategories = async(req, res) => {
+  try {
+    const limitNumber = 20;
+    const categories = await Category.find({}).limit(limitNumber);
+    res.render('categories', { title: 'Cooking Blog - Categoreis', categories } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
+  }
+} 
+
+
+/**
+ * GET /categories/:id
+ * Categories By Id
+*/
+exports.exploreCategoriesById = async(req, res) => { 
+  try {
+    let categoryId = req.params.id;
+    const limitNumber = 20;
+    const categoryById = await Recipe.find({ 'category': categoryId }).limit(limitNumber);
+    res.render('categories', { title: 'Cooking Blog - Categoreis', categoryById } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
+  }
+} 
+ 
+/**
+ * GET /recipe/:id
+ * Recipe 
+*/
+exports.exploreRecipe = async(req, res) => {
+  try {
+    let recipeId = req.params.id;
+    const recipe = await Recipe.findById(recipeId);
+    res.render('recipe', { title: 'Cooking Blog - Recipe', recipe } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
+  }
+} 
+
+
+/**
+ * POST /search
+ * Search 
+*/
+exports.searchRecipe = async(req, res) => {
+  try {
+    let searchTerm = req.body.searchTerm;
+    let recipe = await Recipe.find( { $text: { $search: searchTerm, $diacriticSensitive: true } });
+    res.render('search', { title: 'Cooking Blog - Search', recipe } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
+  }
+  
+}
+
+/**
+ * GET /explore-latest
+ * Explplore Latest 
+*/
+exports.exploreLatest = async(req, res) => {
+  try {
+    const limitNumber = 20;
+    const recipe = await Recipe.find({}).sort({ _id: -1 }).limit(limitNumber);
+    res.render('explore-latest', { title: 'Cooking Blog - Explore Latest', recipe } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
+  }
+} 
+
+
+
+/**
+ * GET /explore-random
+ * Explore Random as JSON
+*/
+exports.exploreRandom = async(req, res) => {
+  try {
+    let count = await Recipe.find().countDocuments();
+    let random = Math.floor(Math.random() * count);
+    let recipe = await Recipe.findOne().skip(random).exec();
+    res.render('explore-random', { title: 'Cooking Blog - Explore Latest', recipe } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
+  }
+} 
+
+/**
+ * GET /about-recipe
+ * About Recipe
+*/
+exports.aboutRecipe = async(req, res) => {
+  res.render('about');
+}
+
+/**
+ * GET /about-recipe
+ * About Recipe
+*/
+exports.contactRecipe = async(req, res) => {
+  res.render('contact');
+}
+
+/**
+ * GET /submit-recipe
+ * Submit Recipe
+*/
+exports.submitRecipe = async(req, res) => {
+  const infoErrorsObj = req.flash('infoErrors');
+  const infoSubmitObj = req.flash('infoSubmit');
+  res.render('submit-recipe', { title: 'Cooking Blog - Submit Recipe', infoErrorsObj, infoSubmitObj  } );
+}
+
+/**
+ * POST /submit-recipe
+ * Submit Recipe
+*/
+exports.submitRecipeOnPost = async(req, res) => {
+  try {
+
+    let imageUploadFile;
+    let uploadPath;
+    let newImageName;
+
+    if(!req.files || Object.keys(req.files).length === 0){
+      console.log('No Files were uploaded.');
+    } else {
+
+      imageUploadFile = req.files.image;
+      newImageName = Date.now() + imageUploadFile.name;
+
+      uploadPath = require('path').resolve('./') + '/public/uploads/' + newImageName;
+
+      imageUploadFile.mv(uploadPath, function(err){
+        if(err) return res.satus(500).send(err);
+      })
+
+    }
+
+    const newRecipe = new Recipe({
+      name: req.body.name,
+      description: req.body.description,
+      email: req.body.email,
+      ingredients: req.body.ingredients,
+      category: req.body.category,
+      image: newImageName
+    });
+    
+    await newRecipe.save();
+
+    req.flash('infoSubmit', 'Recipe has been added.')
+    res.redirect('/submit-recipe');
+  } catch (error) {
+    // res.json(error);
+    req.flash('infoErrors', error);
+    res.redirect('/submit-recipe');
+  }
+}
+
+
+
+
+// Delete Recipe
+// async function deleteRecipe(){
+//   try {
+//     await Recipe.deleteOne({ name: 'New Recipe From Form' });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// deleteRecipe();
+
+
+// Update Recipe
+// async function updateRecipe(){
+//   try {
+//     const res = await Recipe.updateOne({ name: 'New Recipe' }, { name: 'New Recipe Updated' });
+//     res.n; // Number of documents matched
+//     res.nModified; // Number of documents modified
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// updateRecipe();
+
+
+/**
+ * Dummy Data Example 
+*/
+
+// async function insertDymmyCategoryData(){
+//   try {
+//     await Category.insertMany([
+//       {
+//         "name": "Thai",
+//         "image": "thai-food.jpg"
+//       },
+//       {
+//         "name": "American",
+//         "image": "american-food.jpg"
+//       }, 
+//       {
+//         "name": "Chinese",
+//         "image": "chinese-food.jpg"
+//       },
+//       {
+//         "name": "Mexican",
+//         "image": "mexican-food.jpg"
+//       }, 
+//       {
+//         "name": "Indian",
+//         "image": "indian-food.jpg"
+//       },
+//       {
+//         "name": "Spanish",
+//         "image": "spanish-food.jpg"
+//       }
+//     ]);
+//   } catch (error) {
+//     console.log('err', + error)
+//   }
+// }
+
+// insertDymmyCategoryData();
+
+
+// async function insertDymmyRecipeData(){
+//   try {
+//     await Recipe.insertMany([
+//       { 
+//         "name": "Fried Chcken",
+//         "description": `Fried chicken has been described as being "crunchy" and "juicy", as well as "crispy"`,
+//         "email": "recipeemail@food.com",
+//         "ingredients": [
+//           "1 level teaspoon baking powder",
+//           "1 level teaspoon cayenne pepper",
+//           "4 pieces of chicken legs",
+//         ],
+//         "category": "American", 
+//         "image": "Fried-chicken.jpg"
+//       },
+//       { 
+//         "name": "Gyros",
+//         "description": `a sandwich especially of lamb and beef, tomato, onion, and yogurt sauce on pita bread.`,
+//         "email": "americarecipeemail@food.com",
+//         "ingredients": [
+//           "1 level teaspoon baking powder",
+//           "1 level teaspoon cayenne pepper",
+//           "tomoto onion and yogurt",
+//         ],
+//         "category": "American", 
+//         "image": "gyros.jpg"
+//       },
+//       { 
+//         "name": "Doughnuts",
+//         "description": `Doughnuts are usually deep fried from a flour dough, but other types of batters can also be used.`,
+//         "email": "ameriarecipeemail@food.com",
+//         "ingredients": [
+//           "1 level teaspoon baking powder",
+//           "1 level teaspoon cayenne pepper",
+//         ],
+//         "category": "American", 
+//         "image": "bun-nuts.jpg"
+//       },
+//       { 
+//         "name": "Burger",
+//         "description": ` a popular sandwich made from ground meats that are formed into a patty, cooked, and placed between two halves of a bun.`,
+//         "email": "ameriarecipeemail@food.com",
+//         "ingredients": [
+//           "1 level teaspoon baking powder",
+//           "1 level teaspoon cayenne pepper",
+//         ],
+//         "category": "American", 
+//         "image": "Burger.jpg"
+//       },
+//       { 
+//         "name": "Burrito",
+//         "description": `Seriously satisfying, the Burrito is a soft, warm tortilla wrapped.`,
+//         "email": "chineserecipeemail@food.com",
+//         "ingredients": [
+//           "1 level teaspoon baking powder",
+//           "1 level teaspoon cayenne pepper",
+//           "1 level teaspoon hot smoked paprika",
+//         ],
+//         "category": "Chinese", 
+//         "image": "burrito.jpg"
+//       },
+//       { 
+//         "name": "Noodles",
+//         "description": `Noodle texture can vary greatly from hard bite to soft bite depending upon the noodle type.`,
+//         "email": "chineserecipeemail@food.com",
+//         "ingredients": [
+//           "1 level teaspoon baking powder",
+//           "1 level teaspoon cayenne pepper",
+//           "1 level teaspoon hot smoked paprika",
+//         ],
+//         "category": "Chinese", 
+//         "image": "noodles.jpg"
+//       },
+//       { 
+//         "name": "Prawns-Special",
+//         "description": `Tiger and king prawns are both available cooked, often with their heads and shells.`,
+//         "email": "chineserecipeemail@food.com",
+//         "ingredients": [
+//           "1 level teaspoon baking powder",
+//           "1 level teaspoon cayenne pepper",
+//           "1 level teaspoon hot smoked paprika",
+//         ],
+//         "category": "Chinese", 
+//         "image": "seafood.jpg"
+//       },
+//       { 
+//         "name": "Momos",
+//         "description": `Momo is a type of steamed dumpling with some form of filling, most commonly beef and it is originally from Tibet.`,
+//         "email": "chineserecipeemail@food.com",
+//         "ingredients": [
+//           "1 level teaspoon baking powder",
+//           "1 level teaspoon cayenne pepper",
+//           "1 level teaspoon hot smoked paprika",
+//         ],
+//         "category": "Chinese", 
+//         "image": "momo.jpg"
+//       },
+//     ]);
+//   } catch (error) {
+//     console.log('err', + error)
+//   }
+// }
+
+// insertDymmyRecipeData();
+
